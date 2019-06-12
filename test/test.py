8009a95a633bf5ce0ece9fce6e18d5f348d5d383
10120869 <77029478@163.com>
2019-06-12 18:19:22 +0800

    test win

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-# import xml.dom.minidom as xmldom test testmac
+# import xml.dom.minidom as xmldom test testmac testwin
 import os
 from bs4 import BeautifulSoup
 # from xml.etree import ElementTree
